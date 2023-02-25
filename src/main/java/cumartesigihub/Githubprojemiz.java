c1b60b10c57323b9b1763c92b68d0162ddaf9a4a
findik gencoglu <mustafakepenek@gmail.com>
2023-02-25 17:08:02 +0100

    v2

--- src/main/java/cumartesigihub/Githubprojemiz.java
+++ src/main/java/cumartesigihub/Githubprojemiz.java
@@ -6,6 +6,8 @@ public class Githubprojemiz {
         System.out.println("Merhaba baba");
         System.out.println("Merhaba baba geldin mi ?");
         System.out.println("Merhaba baba geldin mi ? telefob bakacaktin ");
+        System.out.println("Merhaba baba geldin mi ?  nasil  oldu  baba  kac para " +"");
+
 
 
 
